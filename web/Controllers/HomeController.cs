7a5d4f4d07b3d8050b311b038325669253005fb6
Capybara77 <andrei7318@yandex.ru>
2022-11-25 16:24:12 +0500

    убрал лишнее

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -36,4 +36,26 @@ public class HomeController : Controller
     {
         return View(DataDb.Feedbacks.ToList());
     }
+
+    public IActionResult Buy(int id)
+    {
+        if (id == 1)
+            return View((object)"Роутер xiaomi");
+
+        else if (id == 2)
+            return View((object)"Роутер TP-Link");
+
+        return NotFound();
+    }
+
+    public async Task<IActionResult> SubmitBuy(string name, string email, string pname)
+    {
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email) ||
+            string.IsNullOrEmpty(pname))
+            return BadRequest();
+
+        string text = $"Здравствуйте, {name}. Ваш заказ - {pname} (1 шт.) успешно оформлен, спасибо за обращение.";
+        await Email.SendEmailAsync(email, "Покупка товара на сайте Mybeeline", text);
+        return View("success");
+    }
 }
