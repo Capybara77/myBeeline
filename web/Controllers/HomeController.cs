88fc7f5b3d29525a3cc520a2fb94bf058b382ab2
Capybara77 <andrei7318@yandex.ru>
2022-11-25 16:09:06 +0500

    подключил почту. настроил форму отправки

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using web.Models;
+using web.Tools;
 
 namespace web.Controllers;
 
@@ -35,5 +36,32 @@ public class HomeController : Controller
     public IActionResult Data()
     {
         return View(DataDb.Feedbacks.ToList());
+    }
+
+    public string Send()
+    {
+        return Email.SendEmailAsync("andrei7318@yandex.ru", "text", "text2").Result.ToString();
+    }
+
+    public IActionResult Buy(int id)
+    {
+        if (id == 1)
+            return View((object)"Роутер xiaomi");
+
+        else if (id == 2)
+            return View((object)"Роутер TP-Link");
+
+        return NotFound();
+    }
+
+    public async Task<IActionResult> SubmitBuy(string name, string email, string pname)
+    {
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email) ||
+            string.IsNullOrEmpty(pname))
+            return BadRequest();
+
+        string text = $"Здравствуйте, {name}. Ваш заказ - {pname} (1 шт.) успешно оформлен, спасибо за обращение.";
+        await Email.SendEmailAsync(email, "Покупка товара на сайте Mybeeline", text);
+        return View("success");
     }
 }
---
+++ web/Tools/Email.cs
@@ -0,0 +1,50 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace web.Tools
+{
+    public static class Email
+    {
+        private static string _emailSender;
+        private static string _password;
+
+        static Email()
+        {
+            if (File.Exists("email"))
+                _emailSender = File.ReadAllText("email");
+
+            if (File.Exists("password"))
+                _password = File.ReadAllText("password");
+        }
+
+        public static async Task<bool> SendEmailAsync(string emailGetter, string subject, string text)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта Mybeeline", _emailSender));
+            emailMessage.To.Add(new MailboxAddress("Beeline", emailGetter));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = text
+            };
+
+            using var client = new SmtpClient();
+
+            await client.ConnectAsync("smtp.yandex.ru", 25, false);
+            await client.AuthenticateAsync(_emailSender, _password);
+            await client.SendAsync(emailMessage);
+
+            try
+            {
+                await client.DisconnectAsync(true);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
